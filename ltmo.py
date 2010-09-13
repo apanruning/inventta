eb01226bfb79d5b805bcde786d8d9ee30272fe8c
Matias Iturburu <maturburu@gmail.com>
2010-09-13 17:40:39 -0300

    casi a par con la versión django, faltan los rss y los tags

--- ltmo.py
+++ ltmo.py
@@ -3,14 +3,17 @@
 import os
 import json
 import settings
+from couchdb import Server
 from flask import Flask
 from flask import request, session, g, redirect, url_for, render_template, flash
-#from flaskext.couchdb import CouchDBManager, ViewDefinition
-from couchdb import Server
+from flaskext.markdown import Markdown
+from uuid import uuid4
+
 
 app = Flask('ltmo', static_path='/media')
 app.config.from_object(settings)
 
+Markdown(app)
 couch_server = Server(app.config['COUCHDB_SERVER'])
 
 try :
@@ -22,14 +25,17 @@ except KeyError:
 def about():
     f = open(os.path.join(app.config['BASE_DIR'], 'ABOUT'), 'r')
     return {
-        'about':str(f.read()),
+        'about':f.read().decode('utf-8'),
     }
     
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    func_str = "function(d) { if (d.description.length>4) emit(d.description,null);}"
-    result = db.query(func_str)
+    func_str = "function(d) { if (d.description.length>4) emit(d,null);}"
+    rows = db.view('_all_docs', include_docs=True)
+    object_list = [row.doc for row in rows]
     if request.method == 'POST':
-       leak = db.create(**json.loads(request.data))
-       
-    return render_template('index.html', object_list=result)
+        doc_id = uuid4().hex
+        db[doc_id] = json.loads(request.data)      
+        return render_template('success.json')
+         
+    return render_template('index.html', object_list=object_list)
