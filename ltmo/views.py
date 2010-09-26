bf0d9b4a8c41cd520e9555f4bee8e33aadfa0001
Matias Iturburu <maturburu@gmail.com>
2010-09-26 17:28:43 -0300

    probando el form, no hace como quería, me preparo a hacer la gran partida

--- ltmo/views.py
+++ ltmo/views.py
@@ -6,17 +6,23 @@ from markdown import markdown
 from werkzeug.contrib.atom import AtomFeed
 from flask import request, session, redirect, url_for, render_template, flash
 from ltmo import app
-from ltmo.models import Leak, db
+from ltmo.models import db, Leak, Tag
 from ltmo.forms import LeakForm
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    form = LeakForm()
-    object_list = Leak.query.order_by('created')
+    form = None
+    object_list = Leak.query.order_by('-created')
     if request.method == 'POST':
         data = json.loads(request.data)
-        leak = Leak(**data)
+        form = LeakForm(data)
+        if form.is_valid:
+            leak = Leak(**form.data)
+            for x in form.data['tags']:
+                import ipdb; ipdb.set_trace()
+                tag = Tag.get_or_create(x)
+                db.session.add(tag)
         db.session.add(leak)
         db.session.commit()
         return render_template('success.json')
