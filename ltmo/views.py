6c92a492c7add603925e85e1a7b2bf96b48ed319
Matias Iturburu <maturburu@gmail.com>
2010-09-26 15:40:32 -0300

    feeds

--- ltmo/views.py
+++ ltmo/views.py
@@ -2,20 +2,42 @@
 
 import os
 import json
-
+from markdown import markdown
+from werkzeug.contrib.atom import AtomFeed
 from flask import request, session, g, redirect, url_for, render_template, flash
 from ltmo import app
 from ltmo.models import Leak, db
+from ltmo.forms import LeakForm
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    object_list = Leak.query.all()
+    form = LeakForm()
+    object_list = Leak.query.order_by('created')
+    import ipdb; ipdb.set_trace()
     if request.method == 'POST':
         data = json.loads(request.data)
         leak = Leak(**data)
         db.session.add(leak)
-        import ipdb; ipdb.set_trace()
         db.session.commit()
         return render_template('success.json')
          
-    return render_template('index.html', object_list=object_list)
+    return render_template('index.html', object_list=object_list, form=form)
+
+@app.route('/feed')
+def atom_feed():
+    feed = AtomFeed("Derrames publicados", feed_url=request.url,
+                    url=request.host_url,)
+
+    for leak in Leak.query.order_by('created'):
+        leak_url = '%s%s' %(request.host_url, leak.id)
+        feed.add(
+            leak.title, 
+            markdown(leak.description), 
+            content_type='html',
+            author=leak.author, 
+            url=leak_url, 
+            id=leak.id,
+            updated=leak.created, 
+        )
+    return feed.get_response()
+
