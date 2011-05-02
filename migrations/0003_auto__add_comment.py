a1a61ed79f3d96560c901c6e502f6cd83d1c4bad
tutuca <maturburu@gmail.com>
2011-05-01 21:49:45 -0300

    Formulario y migracion para los comentarios

---
+++ migrations/0003_auto__add_comment.py
@@ -0,0 +1,47 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Comment'
+        db.create_table('inventta_comment', (
+            ('idea_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['inventta.Idea'], unique=True, primary_key=True)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['inventta.Idea'], null=True)),
+        ))
+        db.send_create_signal('inventta', ['Comment'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Comment'
+        db.delete_table('inventta_comment')
+
+
+    models = {
+        'inventta.comment': {
+            'Meta': {'object_name': 'Comment', '_ormbases': ['inventta.Idea']},
+            'idea_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['inventta.Idea']", 'unique': 'True', 'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventta.Idea']", 'null': 'True'})
+        },
+        'inventta.idea': {
+            'Meta': {'object_name': 'Idea'},
+            'author': ('django.db.models.fields.SlugField', [], {'default': "'anon'", 'max_length': '20', 'db_index': 'True'}),
+            'changed': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_draft': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'metadata': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'rendered': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'db_index': 'True', 'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'tags': ('tagging.fields.TagField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '126', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['inventta']
